6821adcfca8469eb725b04ba3f5b8d6078c10752
simon <simon.blondieau@hotmail.fr>
2022-04-21 11:36:40 +0200

    now it's possible to use arguments to set the max number of points scattered along the graph

--- polygon_graphical_test.py
+++ polygon_graphical_test.py
@@ -1,4 +1,5 @@
 import random
+import argparse
 from typing import Final
 
 from matplotlib import pyplot
@@ -16,37 +17,49 @@ SIZE_Y_AXIS: Final = 1
 # Results path
 PDF_PATH: Final = 'output/points_in_polygon.pdf'
 
-# Number of random points
-COUNT_POINTS: Final = 10000
 
-# The polygon
-polygon = Polygon([(0.1, 0.3), (0.1, 0.6), (0.4, 0.7), (0.8, 0.7), (0.8, 0.3), (0.5, 0.5)])
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--points', help='Number of points to test.', type=int)
+    parser.add_argument('-r', help='Record the result to a PDF file instead of just showing it on screen', action='store_true')
+    args = parser.parse_args()
 
-print('Prepare the pyplot figure...')
-pyplot.xlabel('x')
-pyplot.ylabel('y')
-figure = pyplot.figure()
-axes = figure.add_subplot(1, 1, 1, aspect='equal')
-axes.set_title(f'Test the point_in_polygon function with \n {polygon}')
-axes.set_xlim(0, SIZE_X_AXIS)
-axes.set_ylim(0, SIZE_Y_AXIS)
+    # Number of random points
+    count_points: Final = args.points or 1000
 
-print('Drawing the polygon...')
-axes.add_patch(PolygonPatch(polygon.exterior.coords, linewidth=1, edgecolor='k', facecolor='none'))
+    # The polygon TODO: randomize this (can be hard to do though...)
+    polygon = Polygon([(0.1, 0.3), (0.1, 0.6), (0.4, 0.7), (0.8, 0.7), (0.8, 0.3), (0.5, 0.5)])
 
-print('Plotting the point...')
-with alive_bar(COUNT_POINTS, bar='filling', spinner='waves') as bar:
-    for i in range(COUNT_POINTS):
-        x = random.uniform(0, SIZE_X_AXIS)
-        y = random.uniform(0, SIZE_Y_AXIS)
-        point = Point(x, y)
-        if polygon.contains(point):
-            pyplot.plot(x, y, '.g')
-        else:
-            pyplot.plot(x, y, '.r')
-        bar()
+    print('Prepare the pyplot figure...')
+    figure = pyplot.figure()
+    axes = figure.add_subplot(1, 1, 1, aspect='equal')
+    axes.set_title(f'Test the point_in_polygon function with \n {polygon}')
+    axes.set_xlim(0, SIZE_X_AXIS)
+    axes.set_ylim(0, SIZE_Y_AXIS)
 
-print(f'Saving results in {PDF_PATH}')
-figure.savefig(PDF_PATH)
+    print('Drawing the polygon...')
+    axes.add_patch(PolygonPatch(polygon.exterior.coords, linewidth=1, edgecolor='k', facecolor='none'))
 
-print('Done!')
+    print('Plotting the point...')
+    with alive_bar(count_points, bar='filling', spinner='waves') as bar:
+        for _ in range(count_points):
+            x = random.uniform(0, SIZE_X_AXIS)
+            y = random.uniform(0, SIZE_Y_AXIS)
+            point = Point(x, y)
+            if polygon.contains(point):
+                pyplot.plot(x, y, '.g')
+            else:
+                pyplot.plot(x, y, '.r')
+            bar()
+
+    if args.r:
+        print(f'Saving results in {PDF_PATH}')
+        figure.savefig(PDF_PATH)
+    else:
+        pyplot.show()
+
+    print('Done!')
+
+
+if __name__ == '__main__':
+    main()
